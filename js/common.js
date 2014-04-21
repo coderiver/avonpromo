40929fc56b850d2cba6b0c9085bad2135322a652
akella <akella.a@gmail.com>
2014-04-21 13:34:21 +0300

    fixes

--- js/common.js
+++ js/common.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
-
+	$('.code input').keyup(function(e){
+			if((e.keyCode > 64 && e.keyCode < 91) || (e.keyCode > 47 && e.keyCode < 58)) {
+				$(this).next('input').focus();
+			}
+		});
 });
